76bd3afe0918fa3f4377500bd1ce50499b4f774c
unknown <milosftnsavic@gmail.com>
2024-11-09 23:01:14 +0100

    sredjen i footer da prebacuje na mapu kada se klikne

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,15 +2,24 @@ import { Link } from "../Link/Link";
 import "./Footer.css";
 import wallet from "../images/wallet.png";
 import zuttaxi from "../images/zuttaxi.png";
+import { useNavigate } from "react-router-dom";
 export function Footer() {
+  const navigate = useNavigate();
   let footer = <>
 <div className='footerItem'>
               <img src={wallet} className='footerImage'></img>
               <p className='footerText'>WALLET</p>
               </div>
               <div className='footerItem'>
-              <img src={zuttaxi} className='footerImage'></img>
-              <p className='footerText'>FIND</p>
+              <img src={zuttaxi} 
+              className='footerImage'
+               onClick={() => navigate('/find-ride')}
+               alt='Taxi'>
+
+               </img>
+              <p className='footerText'
+               onClick={() => navigate('/find-ride')}
+               >FIND</p>
               </div>
   </>;
   return <footer className="footer">{footer}</footer>;
